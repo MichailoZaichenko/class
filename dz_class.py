0c5c018371a20a02ea9b9dcf9538194a4bd0ce14
MichailoZaichenko <michailo.zaichenko@gmail.com>
2022-12-14 16:57:49 +0200

    class Area

--- dz_class.py
+++ dz_class.py
@@ -1,12 +1,13 @@
-# class Area:
-#     def __init__(self, area):
-#         self.area = area
+class Area:
+    def __init__(self, name):
+        self.name = name
 
-class Square():
-    def __init__(self, side_a):
+class Square(Area):
+    def __init__(self, name, side_a):
+        super().__init__(name)
         self.side_a = side_a
-
+        
     def area(self):
-        print(f"Area of square is {self.side_a**2}")
-tr = Square(2)
+        print(f"Area of {self.name} is {self.side_a**2}")
+tr = Square("squere",3)
 tr.area()
