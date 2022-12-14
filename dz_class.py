ca2f7c559aa8f25bd527c3c82521c9d8e1ad76c2
MichailoZaichenko <michailo.zaichenko@gmail.com>
2022-12-14 17:18:21 +0200

    class Circle

--- dz_class.py
+++ dz_class.py
@@ -16,8 +16,21 @@ class Rectangle(Square):
         self.side_b = side_b
 
     def area(self):
-        print(f"Area of {self.name} is {self.side_a*self.side_b}") 
+        print(f"Area of {self.name} is {self.side_a*self.side_b}")
+
+class Circle(Area):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+
+    def area(self):
+        print(f"Area of {self.name} is {3.14*self.radius**2}")
+
+
+    
 square = Square("squere",3)
 square.area()
 rectangle = Rectangle("rectangle",3, 4)
 rectangle.area()
+circle = Circle("circle",10)
+circle.area()
